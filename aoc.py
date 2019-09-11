08e1010eed199f0ef9ec674897cc28b33732fc8a
Jorge Abraham Serena Aranda <abraham.serena.lk@gmail.com>
2019-09-11 11:15:00 -0500

    Update Class 'Ciudad'

--- aoc.py
+++ aoc.py
@@ -1,71 +1,45 @@
 import numpy
 import matplotlib.pyplot as plt
-import networkx as nx
 
 
+class Ciudad:
+    '''
+    Clase contenedora de distancia entre ciudades, visibilidad y feromonas.
+    '''
+    def crearCiudad(self, nCity):
+        print(nCity)
+
+class Hormiga:
+    '''
+    Clase contenedora de creación y caminos de hormigas.add()
+    '''
 if __name__ == "__main__":
 
-    matrizDistancia = [
-        [0,8,7,0,3,0,0,0,0,7,5],
-        [8,0,0,0,0,4,0,0,4,5,4],
-        [7,0,0,0,6,0,0,7,0,0,0], 
-        [0,0,0,0,0,0,2,0,3,0,0],
-        [3,0,6,0,0,5,0,5,0,0,4],
-        [0,4,0,0,5,0,3,5,3,0,1],
-        [0,0,0,2,0,3,0,5,3,0,0],
-        [0,0,7,0,5,5,5,0,0,0,0],
-        [0,4,0,3,0,3,3,0,0,0,0],
-        [7,5,0,0,0,0,0,0,0,0,0],
-        [5,4,0,0,4,1,0,0,0,0,0]
-    ]
+    aguascalientes = Ciudad.crearCiudad(11)
+    # matrizDistancia = [
+    #     [0,8,7,0,3,0,0,0,0,7,5],
+    #     [8,0,0,0,0,4,0,0,4,5,4],
+    #     [7,0,0,0,6,0,0,7,0,0,0], 
+    #     [0,0,0,0,0,0,2,0,3,0,0],
+    #     [3,0,6,0,0,5,0,5,0,0,4],
+    #     [0,4,0,0,5,0,3,5,3,0,1],
+    #     [0,0,0,2,0,3,0,5,3,0,0],
+    #     [0,0,7,0,5,5,5,0,0,0,0],
+    #     [0,4,0,3,0,3,3,0,0,0,0],
+    #     [7,5,0,0,0,0,0,0,0,0,0],
+    #     [5,4,0,0,4,1,0,0,0,0,0]
+    # ]
 
-    matrizFeromonas = [
-        [0   ,0.01,0.01,0   ,0.01,0   ,0   ,0   ,0   ,0.01,0.01],
-        [0.01,0   ,0   ,0   ,0   ,0.01,0   ,0   ,0.01,0.01,0.01],
-        [0.01,0   ,0   ,0   ,0.01,0   ,0   ,0.01,0   ,0   ,0   ],
-        [0   ,0   ,0   ,0   ,0   ,0   ,0.01,0   ,0.01,0   ,0   ],
-        [0.01,0   ,0.01,0   ,0   ,0.01,0   ,0.01,0   ,0   ,0.01],
-        [0   ,0.01,0   ,0   ,0.01,0   ,0.01,0.01,0.01,0   ,0   ],
-        [0   ,0   ,0   ,0.01,0   ,0.01,0   ,0.01,0.01,0   ,0   ],
-        [0   ,0   ,0.01,0   ,0.01,0.01,0.01,0   ,0   ,0   ,0   ],
-        [0   ,0.01,0   ,0.01,0   ,0.01,0.01,0   ,0   ,0   ,0   ],
-        [0.01,0.01,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ],
-        [0.01,0.01,0   ,0   ,0.01,0.01,0   ,0   ,0   ,0   ,0   ]
-    ]
+    # rows, cols = numpy.where(matrizDistancia)
+    # edges = zip(rows.tolist(), cols.tolist())
+    # gr = nx.Graph()
+    # gr.add_edges_from(edges)
+    # labelsVec = {0:'1', 1:'2', 2:'3',3:'4',4:'5',
+    #     5:'6',6:'7',7:'8',8:'9',9:'10',10:'11'}
+    # plt.title('Conexión entre ciudades.') 
+    # nx.draw(gr, labels=labelsVec, with_labels=True)
+    # plt.show()
 
-    matrizVisibilidad = [
-        [0  ,1/8,1/7,0  ,1/3,0  ,0  ,0  ,0  ,1/7,1/5],
-        [1/8,0  ,0  ,0  ,0  ,1/4,0  ,0  ,1/4,1/5,1/4],
-        [1/7,0  ,0  ,0  ,1/6,0  ,0  ,1/7,0  ,0  ,0  ],
-        [0  ,0  ,0  ,0  ,0  ,0  ,1/2,0  ,1/3,0  ,0  ],
-        [1/3,0  ,1/6,0  ,0  ,1/5,0  ,1/5,0  ,0  ,1/4],
-        [0  ,1/4,0  ,0  ,1/5,0  ,1/3,1/5,1/3,0  ,1/1],
-        [0  ,0  ,0  ,1/2,0  ,1/3,0  ,1/5,1/3,0  ,0  ],
-        [0  ,0  ,1/7,0  ,1/5,1/5,1/5,0  ,0  ,0  ,0  ],
-        [0  ,1/4,0  ,1/5,0  ,1/3,1/3,0  ,0  ,0  ,0  ],
-        [1/7,1/5,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ],
-        [1/5,1/4,0  ,0  ,1/4,1/1,0  ,0  ,0  ,0  ,0  ]
-    ]
-
-
-    rows, cols = numpy.where(matrizDistancia)
-    edges = zip(rows.tolist(), cols.tolist())
-    gr = nx.Graph()
-    gr.add_edges_from(edges)
-    labelsVec = {0:'1', 1:'2', 2:'3',3:'4',4:'5',
-        5:'6',6:'7',7:'8',8:'9',9:'10',10:'11'}
-    plt.title('Conexión entre ciudades.') 
-    nx.draw(gr, labels=labelsVec, with_labels=True)
-
-    plt.show()
-
-    plt.matshow(matrizDistancia)
-    plt.title('Matriz de distancias entre ciudades.')
-    plt.show()
-
-    plt.matshow(matrizFeromonas)
-    plt.title('Matriz de Feromonas entre ciudades.')
-    plt.show()
 
 
 
