133f73544827c5bfb25b47811b0a6dceb361261d
Abraham <abraham.serena.lk@gmail.com>
2019-09-15 14:48:28 -0500

    CreateFeroMap funciton works

--- aoc.py
+++ aoc.py
@@ -16,29 +16,51 @@ import matplotlib.pyplot as plt
     #     [5,4,0,0,4,1,0,0,0,0,0]
     # ]
 
-class Mapa:
+class Map:
     '''Clase contenedora de las ciudades, así como la distancia entre ellas,
     feremonas, así como la visibilidad.
     '''
     def __init__(self, nCity):
         self.nCity = nCity
-        self.cityDistance = numpy.random.randint(0,nCity,size=(nCity,nCity))
-        for i in range(nCity):
-            for j in range(i+1, nCity):
+    
+    def createCityMap(self):
+        self.cityDistance = numpy.random.randint(0,self.nCity,size=(self.nCity, self.nCity))
+        for i in range(self.nCity):
+            for j in range(i + 1, self.nCity):
                 if self.cityDistance[i,j] != self.cityDistance[j,i]:
                     temp = self.cityDistance[i,j]
                     self.cityDistance[j,i] = temp
         numpy.fill_diagonal(self.cityDistance,0)
-        self.visibilityMap = self.cityDistance
-        self.visibilityMap = numpy.divide(1,self.cityDistance)
-               
+
+    def createFeroMap(self):
+        self.feroMap = numpy.zeros([self.nCity, self.nCity])
+        for i in range(self.nCity):
+            for j in range(i + 1, self.nCity):
+                if self.cityDistance[i,j] != 0:
+                    self.feroMap[i,j] = 0.01
+                    self.feroMap[j,i] = 0.01
+
+
+
+
+class Ant:
+    '''
+    Clase contenedora para el recorrido de las hormigas, así como
+    depositar feromonas en el camino.
+    '''
+    def __init__(self, nAnt):
+        self.nAnt = nAnt
+
+def main():
+    aguascalientes = Map(11)   
+    aguascalientes.createCityMap() 
+    aguascalientes.createFeroMap()
+    print(aguascalientes.cityDistance)
+    print('---------------------------')
+    print(aguascalientes.feroMap)
 
 if __name__ == "__main__":
-
-
-    aguascalientes = Mapa(11)
-    print(aguascalientes.cityDistance)
-    print(aguascalientes.visibilityMap)
+    main()
 
     # rows, cols = numpy.where(matrizDistancia)
     # edges = zip(rows.tolist(), cols.tolist())
